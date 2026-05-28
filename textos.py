92bf250134ce20569a1cf9114d08bcf648254973
CarmenRu <cra1009@alu.ubu.es>
2026-05-28 15:03:12 +0200

    Interacciones y textos, queda ATC

--- textos.py
+++ textos.py
@@ -48,6 +48,14 @@ def texto_intro (ppio, enzimas, principales, primero=False):
                     print()
 
 
+
+
+
+
+
+
+
+#SE MUESTRAN SOLO LAS INTERACCIONES EN LA PRIMERA INGESTA, PONER ESO
 def texto_acciones (ppio_1, acc_1, ppio_2, acc_2, enzima):
     """
     Se describe la interaccion solo con la enzima concreta que se pasa por parámetro
@@ -67,7 +75,7 @@ def texto_acciones (ppio_1, acc_1, ppio_2, acc_2, enzima):
     #Declaramos cuales son las posibilidades
     posibilidades = ["inducer", "substrate", "inhibitor"]
     #Print explicativo
-    print(f"En este caso solo se describen las interacciones de los principios activos con la enzima: {enzima} teniendo en cuenta solo que lleven a cabe algunas de las siguientes acciones: {posibilidades}")
+    print(f"En este caso solo se describen las interacciones de los principios activos con la enzima: {enzima} teniendo en cuenta solo que lleven a cabo algunas de las siguientes acciones: {posibilidades}")
     print()
     
     #De las listas proporcionadas con las acciones UNICAS eliminamos las que no tenemos en cuenta:
@@ -75,6 +83,73 @@ def texto_acciones (ppio_1, acc_1, ppio_2, acc_2, enzima):
     mias_2 = [x for x in acc_2 if x in posibilidades]
 
     #Guardamos una variable con las coincidencias de las acciones (sustrato-sustrato, inhib-inhib, induct-induct)
+    #Se supone que ya deben de ser las distintas pero porsiacaso set
     doble = list(set(mias_1) & set(mias_2))
 
     #Una vez declaramos las variables comprobamos cada uno de los casos para obtener el texto adecuado
+    #Aunque se ha comprobado que no hay elementos nulos en las acciones cuando la BBDD queda reducida a solo los que tienen enzimas se comprueba
+    if (acc_1==None) or (acc_2==None):
+        print(f"Las acciones de la enzima {enzima} para los principios activos {ppio_1} y {ppio_2} no están registradas en DrugBank, por tanto no se puede describir la interacción que tienen.")
+        print()
+        
+    else:
+        #De la lista ninguna es inductor-inhibidor-sustrato
+        if (len(mias_1)==0) or (len(mias_2)==0):
+            print(f"De las acciones que se llevan a cabo con la enzima: {e} en los dos principios activos consulltados: {ppio_1} y {ppio_2} ninguna es de las tenidas en cuenta, por tanto no se puede describir la interacción.")
+            print()
+
+        else:
+            #Mostrar las posibilidades
+            if doble:
+                #Es posible que doble tenga mas de una acción ¿QUE HACES EN ESE CASO?
+                print(f"")
+                print()
+            #Sustrato
+            else:
+
+            #PONER TEXTO GENERAL Y DICCIONARIO
+                if "substrate" in acc_1:
+                    
+                    #sustrato-inductor
+                    if "inducer" in acc_2:
+                        print(f"Con la enzima: {e} el {ppio_1} está actuando como sustrato y {ppio_2} está actuando como inductor, en este caso tendría prevalencia el sustrato")
+                        print()
+                    #Se supone que con un else deberia valer
+                    #sustrato-inhibidor
+                    elif "inhibitor" in acc_2:
+                        print(f"")
+                        print()
+    
+                #Inhibidor
+                elif "inhibitor" in acc_1:
+                    
+                    #inhibidor-indcutor
+                    if "inducer" in acc_2:
+                        print(f"")
+                        print()
+                    #Se supone que con un else deberia valer
+                    #Inhibidor-sustrato
+                    elif "substrate" in acc_2:
+                        print(f"")
+                        print()
+    
+                #Inductor (tambien podría ser solo un else en teoría)
+                elif "inducer" in acc_1:
+                    
+                    #inductor-inhibidor
+                    if "inhibitor" in acc_2:
+                        print(f"")
+                        print()
+                    #Se supone que con un else deberia valer
+                    #inductor-sustrato
+                    elif "substrate" in acc_2:
+                        print(f"")
+                        print()
+
+
+
+
+
+#Y NO SERÍA POSIBLE UN TEXTO GENERICO?? PARA LA ENZIMA E EL PPIO ACTIVO  1  ESTA ACTUANDO COMO: TRADUCCION DE SU ACTUACION Y EL DOS COMO: TRADD, EN ESTE CASO PREVALECERIA: (consulto diccionario con interacción?)
+
+#Y SI UNO TIENE DOS ACCIONES????? CON QUE XUXA ME QUEDO
